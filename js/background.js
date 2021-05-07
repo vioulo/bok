0bac4ef0af0030af49e58f7bc36779b47639aadb
eurus <vioulo@outlook.com>
2021-05-07 20:35:54 +0800

    first commit

---
+++ js/background.js
@@ -0,0 +1,10 @@
+// chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+
+// 	sendResponse(123);
+// })
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log('收到来自content-script的消息：');
+    console.log(request, sender, sendResponse);
+    sendResponse('我是后台，我已收到你的消息：' + JSON.stringify(request));
+});
