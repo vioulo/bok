22c8ed799d131d6ec54c18e3504a97ac0188baea
shiny <vioulo@outlook.com>
2022-07-10 17:09:31 +0800

    update omnibox

--- js/background.js
+++ js/background.js
@@ -1,45 +1,40 @@
 chrome.omnibox.onInputChanged.addListener((text, suggest) => {
-	console.log('inputChanged: ' + text);
-	if(!text) return;
-    if(text == '微博') {
-		suggest([
-			{content: '新浪' + text, description: '新浪' + text},
-			{content: '腾讯' + text, description: '腾讯' + text},
-			{content: '搜狐' + text, description: '搜索' + text},
-		]);
-	}
-	else {
-		suggest([
-			{content: '百度搜索 ' + text, description: '百度搜索 ' + text},
-			{content: '谷歌搜索 ' + text, description: '谷歌搜索 ' + text},
-		]);
-	}
+    console.log('inputChanged: ' + text);
+    if (!text) return;
+    chrome.storage.local.get('links', function (res) {
+        let link = res['links'];
+        let sut = [];
+        for (let i in link) {
+            let title = link[i].title.toLowerCase();
+            let desc = link[i].title.replace('&', '&amp;');
+            if (title.indexOf(text) != -1) {
+                sut.push({ content: i, description: desc });
+            }
+        }
+        if (sut.length) {
+            suggest(sut);
+        }
+    });
 });
 
 // 当用户接收关键字建议时触发
-chrome.omnibox.onInputEntered.addListener((text) => {
-    console.log('inputEntered: ' + text);
-	if(!text) return;
-	var href = '';
-    if(text.endsWith('美女')) href = 'http://image.baidu.com/search/index?tn=baiduimage&ie=utf-8&word=' + text;
-	else if(text.startsWith('百度搜索')) href = 'https://www.baidu.com/s?ie=UTF-8&wd=' + text.replace('百度搜索 ', '');
-	else if(text.startsWith('谷歌搜索')) href = 'https://www.google.com.tw/search?q=' + text.replace('谷歌搜索 ', '');
-	else href = 'https://www.baidu.com/s?ie=UTF-8&wd=' + text;
-	openUrlCurrentTab(href);
+chrome.omnibox.onInputEntered.addListener((key) => {
+    if (key == '') return;
+    chrome.storage.local.get('links', function (res) {
+        let link = res['links'];
+        openUrlCurrentTab(link[key].link);
+    });
 });
 // 获取当前选项卡ID
-function getCurrentTabId(callback)
-{
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
-	{
-		if(callback) callback(tabs.length ? tabs[0].id: null);
-	});
+function getCurrentTabId(callback) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (callback) callback(tabs.length ? tabs[0].id : null);
+    });
 }
 
 // 当前标签打开某个链接
-function openUrlCurrentTab(url)
-{
-	getCurrentTabId(tabId => {
-		chrome.tabs.update(tabId, {url: url});
-	})
+function openUrlCurrentTab(url) {
+    getCurrentTabId(tabId => {
+        chrome.tabs.update(tabId, { url });
+    })
 }
