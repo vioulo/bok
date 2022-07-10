a7451344d2b9c0e7e0931e50121275ee11946ac5
shiny <vioulo@outlook.com>
2022-07-10 19:02:10 +0800

    transcode omnibox str

--- js/background.js
+++ js/background.js
@@ -6,7 +6,7 @@ chrome.omnibox.onInputChanged.addListener((text, suggest) => {
         let sut = [];
         for (let i in link) {
             let title = link[i].title.toLowerCase();
-            let desc = link[i].title.replace('&', '&amp;');
+            let desc = transcoding(link[i].title);
             if (title.indexOf(text) != -1) {
                 sut.push({ content: i, description: desc });
             }
@@ -37,4 +37,13 @@ function openUrlCurrentTab(url) {
     getCurrentTabId(tabId => {
         chrome.tabs.update(tabId, { url });
     })
+}
+
+function transcoding(str) {
+    s = str.replace(/&/g, '&amp;');
+    s = s.replace(/</g, '&lt;');
+    s = s.replace(/>/g, '&gt;');
+    s = s.replace(/\'/g, '&#39;');
+    s = s.replace(/\"/g, '&quot;');
+    return s;
 }
